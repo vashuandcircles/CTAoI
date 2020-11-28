1119be7b4774b7a727a988202c578f4f95083dc6
nutan.dahal+2-at-643827904245 </j7RX3op5yrCfZss4yoNpbQGCG3HNeiu64fQzW4uTDs=>
2020-11-28 18:29:53 +0545

    added 9 in pagination.

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -35,8 +35,8 @@ class PageController extends Controller
 
     public function coachings(Request $request)
     {
-        $user = User::where('type', 1)->orderBy('id', 'desc')->paginate(6);
-        $coachings = Coaching::orderBy('userid', 'desc')->paginate(6);
+        $user = User::where('type', 1)->orderBy('id', 'desc')->paginate(9);
+        $coachings = Coaching::orderBy('userid', 'desc')->paginate(9);
         if ($request->ajax()) {
             $view = view('data')->with(compact('user', 'coachings'))->render();
             return response()->json(['html' => $view]);
