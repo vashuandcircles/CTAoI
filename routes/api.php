01f66d57fcd19384394ef865e0d3c4d1f17c106b
nutan.dahal+2-at-643827904245 </j7RX3op5yrCfZss4yoNpbQGCG3HNeiu64fQzW4uTDs=>
2020-12-18 21:10:04 +0545

    working on live class

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+// Get list of meetings.
+Route::get('/meetings', 'Zoom\MeetingController@list');
+
+// Create meeting room using topic, agenda, start_time.
+Route::post('/meetings', 'Zoom\MeetingController@create');
+
+// Get information of the meeting room by ID.
+Route::get('/meetings/{id}', 'Zoom\MeetingController@get')->where('id', '[0-9]+');
+Route::patch('/meetings/{id}', 'Zoom\MeetingController@update')->where('id', '[0-9]+');
+Route::delete('/meetings/{id}', 'Zoom\MeetingController@delete')->where('id', '[0-9]+');
