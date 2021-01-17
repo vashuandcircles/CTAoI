05af43d90f73ad6e3a36f46f2cfe6d018033f873
ojha007 <ojhaamir007@gmail.com>
2021-01-17 19:09:11 +0545

    need help icon

--- resources/views/coaching/zoom-meeting/edit.blade.php
+++ resources/views/coaching/zoom-meeting/edit.blade.php
@@ -4,7 +4,17 @@
     @php($buttonText='Update')
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h3 class="card-title">Zoom meeting Configuration</h3>
+            <span class="card-title">
+                <strong>
+                    Zoom meeting Configuration
+                </strong>
+            </span>
+            <div class="pull-right float-right">
+                <button title="Nee Help to configure ?" class="btn btn-primary">
+                    <i class="fa fa-question"></i> Need Help
+                </button>
+
+            </div>
         </div>
         <div class="card-body">
             <form method="POST" action="{{route('coaching.meetings.configuration.update',$zoomConfig->id)}}">
--- resources/views/teacher/zoom-meeting/create.blade.php
+++ resources/views/teacher/zoom-meeting/create.blade.php
@@ -3,7 +3,17 @@
     @include('messages')
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h3 class="card-title">Zoom meeting Configuration</h3>
+            <span class="card-title">
+                <strong>
+                    Zoom meeting Configuration
+                </strong>
+            </span>
+            <div class="pull-right float-right">
+                <button title="Nee Help to configure ?" class="btn btn-primary">
+                    <i class="fa fa-question"></i> Need Help
+                </button>
+
+            </div>
         </div>
         <div class="card-body">
             <form method="post" action="{{route('teachers.meetings.configuration')}}">
